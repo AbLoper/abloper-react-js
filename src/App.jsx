baf6ecd54c5969c0ab19cb91c9f4185c05c1ebec
AbLoper <abedslayman1982@gmail.com>
2025-12-15 09:00:46 +0200

    preLaunch Copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import LOGO from './assets/Logo.png';
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          backgroundColor: '#020202ff',
+        }}>
+        <img
+          src={LOGO}
+          alt='Abloper Logo'
+          style={{ width: '1000px', height: '300px' }}
+        />
+        <div
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            marginTop: '100px',
+            fontFamily: 'Arial, sans-serif',
+            fontSize: '24px',
+          }}>
+          <p>This Portfolio Website Will Be Launch In 1/1/2026</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
